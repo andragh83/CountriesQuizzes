dd5a04568f173f79a9ae1ae205222a14266e23b9
Andra Ghitulescu <andraghitulescu@andras-mac-mini.local>
2022-02-19 19:10:22 +0000

    adding stuff to prevent errors and app crash

---
+++ src/components/fadeInWrapper.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./animationStyle.css";
+
+const FadeInWrapper = ({ children }) => {
+  return <div className="fadeInClass">{children}</div>;
+};
+
+export default FadeInWrapper;
